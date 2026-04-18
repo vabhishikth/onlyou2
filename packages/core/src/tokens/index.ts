a87eceaa51264929872f1d2f4abbeff2ff17f48d
vabhishikth <v.abhishikth@gmail.com>
2026-04-18 09:06:12 +0530

    feat(phase-2.5a/tokens): add biomarkerPalette + biomarkerFonts (editorial register)

--- packages/core/src/tokens/index.ts
+++ packages/core/src/tokens/index.ts
@@ -1,7 +1,9 @@
-export * from './colors'
-export * from './typography'
-export * from './spacing'
-export * from './radii'
-export * from './shadows'
-export * from './animations'
-export * from './z-index'
+export * from "./colors";
+export * from "./typography";
+export * from "./spacing";
+export * from "./radii";
+export * from "./shadows";
+export * from "./animations";
+export * from "./z-index";
+export { biomarkerPalette, biomarkerFonts } from "./biomarker";
+export type { BiomarkerPaletteKey, BiomarkerFontKey } from "./biomarker";
